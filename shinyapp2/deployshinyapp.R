e19e5f2eb2d5f1826bcc49679d16307bbdc4d2de
Nikita Safronov <safronikita@gmail.com>
2025-10-19 14:33:57 -0400

    Changed shinyapp2 name to Penguins-Analysis-App

--- shinyapp2/deployshinyapp.R
+++
@@ -1,8 +0,0 @@
-library(rsconnect)
-appDir <- normalizePath(".")
-message("Deploying app from: ", appDir)
-rsconnect::deployApp(appDir,
-					 appFiles = c("shinyapp2.R"),
-					 appPrimaryDoc = "shinyapp2.R",
-					 appName = "shinyapp2",
-					 launch.browser = TRUE)
